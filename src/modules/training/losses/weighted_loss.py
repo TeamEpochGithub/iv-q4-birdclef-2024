186aed34adc2d7c9388a1fbf1a250103f0c9d76c
hjdeheer <hugodeheer1234@gmail.com>
2024-05-02 11:45:56 +0200

    Add weighted loss and config to run it with

---
+++ src/modules/training/losses/weighted_loss.py
@@ -0,0 +1,79 @@
+"""Weighted BCE loss implementation, for combating class imbalance in classification tasks."""
+import numpy as np
+import torch
+from torch import nn
+
+
+class WeightedLoss(nn.Module):
+    """Weighted BCE Loss implementation, for combating class imbalance in classification tasks."""
+
+    def __init__(self, weights_path: str) -> None:
+        """Initialize the weighted BCE loss.
+
+        :param weights_path: The path to the weights file.
+
+        """
+        super().__init__()
+        # Push weights to cuda
+
+        self.weights = torch.from_numpy(np.load(weights_path)).cuda()
+
+    # Have an abstract forward
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """Compute the weighted BCE Loss.
+
+        :param inputs: The model predictions.
+        :param targets: The true labels.
+
+        :return: The focal loss.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+
+
+class WeightedBCEWithLogitsLoss(WeightedLoss):
+    """Weighted BCE loss implementation with logits, for combating class imbalance in classification tasks."""
+
+    def __init__(self, weights_path: str) -> None:
+        """Initialize the weighted BCE loss.
+
+        :param weights_path: The path to the weights file.
+
+        """
+        super().__init__(weights_path)
+
+        self.bce = torch.nn.BCEWithLogitsLoss(weight=self.weights)
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """Compute the weighted BCE Loss.
+
+        :param inputs: The model predictions.
+        :param targets: The true labels.
+
+        :return: The focal loss.
+        """
+        # Flatten label and prediction tensors
+        return self.bce(inputs, targets)
+
+
+class WeightedBCELoss(WeightedLoss):
+    """Weighted BCE loss implementation, for combating class imbalance in classification tasks."""
+
+    def __init__(self, weights_path: str) -> None:
+        """Initialize the weighted BCE loss.
+
+        :param weights_path: The path to the weights file.
+
+        """
+        super().__init__(weights_path)
+        self.bce = torch.nn.BCELoss(weight=self.weights)
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """Compute the weighted BCE Loss.
+
+        :param inputs: The model predictions.
+        :param targets: The true labels.
+
+        :return: The focal loss.
+        """
+        # Flatten label and prediction tensors
+        return self.bce(inputs, targets)
