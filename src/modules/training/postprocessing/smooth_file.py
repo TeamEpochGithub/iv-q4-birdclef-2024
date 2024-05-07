119415fd698fd14cbb9866beb6144afd71c89f87
hjdeheer <hugodeheer1234@gmail.com>
2024-05-07 16:18:04 +0200

    Add smooth file function that smooths whole audio files with a factor

--- src/modules/training/postprocessing/smooth_file.py
+++ src/modules/training/postprocessing/smooth_file.py
@@ -11,19 +11,29 @@ from src.modules.training.verbose_training_block import VerboseTrainingBlock
 
 @dataclass
 class SmoothFile(VerboseTrainingBlock):
-    """ Smooth the predictions based on the current 4 minute audio file."""
+    """Smooth the predictions based on the current 4 minute audio file."""
 
-    smooth_factor: float = 0.5
-
+    smooth_factor: float = 0.1
 
     def custom_train(self, x: npt.NDArray[np.float32], y: npt.NDArray[np.float32], **train_args: Any) -> tuple[Any, Any]:
+        """Return the input data and labels."""
+        return x, y
+
+    def custom_predict(self, x: npt.NDArray[np.float32], **pred_args: Any) -> npt.NDArray[np.float32]:
         """Apply smoothing to the predictions.
 
-        :param x: The input data
-        :param y: The target data
-        :return: The predictions and the target data
+        :param x: The input data in shape (n_samples=48n, n_features=182)
+        :return: The predictions
         """
+        # Smooth the predictions based on the current 4 minute audio file (48 samples)
 
+        # Calculate the average of the predictions. y_avg will be of size (n, 182)
+        x_avg = np.zeros((x.shape[0] // 48, x.shape[1]))
+        for i in range(0, x.shape[0], 48):
+            x_avg[i // 48] = np.mean(x[i : i + 48], axis=0)
 
+        # Loop over all samples and apply the smoothing factor
+        for i in tqdm(range(x.shape[0]), desc="Smoothing predictions"):
+            x[i] = x[i] * (1 - self.smooth_factor) + x_avg[i // 48] * self.smooth_factor
 
-
+        return x
