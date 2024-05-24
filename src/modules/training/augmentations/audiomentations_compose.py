ef41917af795b3e5335e0a06dd01b0183d8ffda1
tolgakopar <cahittolgakopar@gmail.com>
2024-05-24 15:57:32 +0200

    fix non-deterministic hash
    All the functions from audiomentations have their memory adresses in their repr...

--- src/modules/training/augmentations/audiomentations_compose.py
+++ src/modules/training/augmentations/audiomentations_compose.py
@@ -1,7 +1,10 @@
 
 from dataclasses import dataclass
+
 from audiomentations import Compose
 import torch
+
+from src.utils.recursive_repr import recursive_repr
 
 @dataclass
 class AudiomentationsCompose:
@@ -13,4 +16,12 @@ class AudiomentationsCompose:
         augmented_x = x.clone()
         for i in range(x.shape[0]):
             augmented_x[i] = torch.from_numpy(self.compose(x[i].squeeze().numpy(), self.sr))
+        print(self)
         return augmented_x
+    
+    def __repr__(self):
+        out = ''
+        for field in self.compose.__dict__['transforms']:
+            out += recursive_repr(field)
+        return out
+
