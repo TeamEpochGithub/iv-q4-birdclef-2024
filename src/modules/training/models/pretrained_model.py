f3fbd84a7b5762dd06aabd6b753171ac32a411d4
Emiel Witting <emielwitting@teamepoch.net>
2024-06-08 18:10:45 +0200

    multiyear fixes

--- src/modules/training/models/pretrained_model.py
+++ src/modules/training/models/pretrained_model.py
@@ -17,7 +17,7 @@ class PretrainedModel(torch.nn.Module):
     model: torch.nn.Module
     crop_head: int | None = None
 
-    def __init__(self, model_path: str | PathLike[str]) -> None:
+    def __init__(self, model_path: str | PathLike[str], crop_head: int | None) -> None:
         """Initialize the model.
 
         :param model_path: The path to the pretrained model to load.
@@ -28,15 +28,15 @@ class PretrainedModel(torch.nn.Module):
         if isinstance(self.model, torch.nn.DataParallel | torch.nn.parallel.DistributedDataParallel):
             self.model = self.model.module
 
+        self.crop_head = crop_head
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass of the model.
 
         :param x: The data of a 4-minute soundscape of shape (48, C, H, W)
         :return: The predictions of shape (48, 182)
         """
-        if self.training:
-            raise NotImplementedError("Fine-tuning a pretrained model is not supported.")
-
+        pred = self.model(x)
         if self.crop_head is not None:
-            x = x[:, :, :, self.crop_head:]
-        return self.model(x)
+            pred = pred[:, :self.crop_head]
+        return pred
